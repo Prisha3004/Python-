d49ac66f9a8449d17ae212a34326564d29ae0536
PrishaMehta <168120755+Prisha3004@users.noreply.github.com>
2026-07-03 08:58:39 +0530

    Delete studentsproject directory

--- studentsproject/students/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Student(models.Model):
-    name=models.CharField()
-    email=models.EmailField()
-    enrollment=models.IntegerField(unique=True,default=None)
-    contact=models.IntegerField(default=None)
