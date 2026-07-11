b7fe802fa5f8bad99c641239d8bf2eb12e202e00
PrishaMehta <168120755+Prisha3004@users.noreply.github.com>
2026-07-11 09:02:19 +0530

    Add files via upload

---
+++ CRMproject/CRM/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=10)
+    city = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
